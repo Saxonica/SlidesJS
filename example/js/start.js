28ac91c17ddec77ae69fa4e4e9d25b81c72ff7ef
Norman Walsh <ndw@nwalsh.com>
2022-07-30 15:54:38 +0100

    Initial commit

---
+++ example/js/start.js
@@ -0,0 +1,71 @@
+window.onload = function() {
+  const defeatTheCache = new Date();
+  SaxonJS.transform({
+    stylesheetLocation: `xslt/slides.sef.json?nocache=${defeatTheCache.getTime()}`,
+    sourceLocation: `index.html?nocache=${defeatTheCache.getTime()}`
+  }, "async");
+};
+
+window.forceHighlight = function() {
+  document.querySelectorAll("pre").forEach(pre => {
+    Prism.highlightElement(pre);
+  });
+};
+
+let meta = document.querySelector("head meta[name='localStorage.key']");
+const localStorageKey = meta && meta.getAttribute("content");
+const notesKey = "viewingNotes";
+let notesView = false;
+
+window.manageSpeakerNotes = {
+  "reload": new Date().toString(),
+  "navigated": false,
+  "currentPage": window.location.href,
+  "reveal": false,
+  "unreveal": false,
+  "showNotes": false,
+  "duration": 'PT0S',
+  "startTime": "",
+  "paused": true
+};
+
+const storageChange = function(changes, areaName) {
+  if (changes.key.startsWith(localStorageKey)) {
+    const key = changes.key.substring(localStorageKey.length + 1);
+    if (window.manageSpeakerNotes[key] !== changes.newValue) {
+      console.log("Change:", key, "=", changes.newValue);
+      window.manageSpeakerNotes[key] = changes.newValue;
+    }
+  }
+};
+
+if (localStorageKey) {
+  const dt = new Date().toString();
+  window.localStorage.setItem(`${localStorageKey}.currentPage`, window.location.href);
+  window.localStorage.setItem(`${localStorageKey}.reveal`, "");
+  window.localStorage.setItem(`${localStorageKey}.unreveal`, "");
+  window.localStorage.setItem(`${localStorageKey}.reload`, dt);
+
+  let duration = window.localStorage.getItem(`${localStorageKey}.duration`);
+  if (duration == null) {
+    window.localStorage.setItem(`${localStorageKey}.duration`, "PT0S");
+  } else {
+    window.manageSpeakerNotes.duration = duration;
+  }
+
+  let startTime = window.localStorage.getItem(`${localStorageKey}.startTime`);
+  if (startTime == null) {
+    startTime = new Date().toISOString();
+    window.localStorage.setItem(`${localStorageKey}.startTime`, startTime);
+  }
+  window.manageSpeakerNotes.startTime = startTime;
+
+  let paused = window.localStorage.getItem(`${localStorageKey}.paused`);
+  if (paused == null) {
+    paused = "true";
+    window.localStorage.setItem(`${localStorageKey}.startTime`, paused);
+  }
+  window.manageSpeakerNotes.paused = (paused == "true");
+
+  window.addEventListener("storage", storageChange);
+};
